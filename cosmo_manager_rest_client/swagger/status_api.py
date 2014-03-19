85a104ce7476c7ac7673209129432d867f3c9dcb
Dan Kilman <dankilman@gmail.com>
2014-03-19 12:45:46 +0200

    CFY-3 add status call to rest client

---
+++ cosmo_manager_rest_client/swagger/status_api.py
@@ -0,0 +1,37 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+__author__ = 'ran'
+
+
+import requests
+import json
+
+
+class StatusApi(object):
+
+    def __init__(self, client):
+        self.api_client = client.api_client
+
+    def status(self):
+        resource_path = '/status'
+        url = self.api_client.resource_url(resource_path)
+
+        response = requests.get(url)
+
+        self.api_client.raise_if_not(200, response, url)
+
+        return self.api_client.deserialize(response.json(),
+                                           'Status')
