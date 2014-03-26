0fafa1b754dd2e6b035922b8242cb766813018ca
Dan Kilman <dankilman@gmail.com>
2014-03-26 16:11:11 +0200

    CFY-72 flake8

--- cosmo_manager_rest_client/cosmo_manager_rest_client.py
+++ cosmo_manager_rest_client/cosmo_manager_rest_client.py
@@ -368,7 +368,7 @@ class CosmoManagerRestClient(object):
         blueprint_name = os.path.basename(os.path.splitext(blueprint_path)[0])
         blueprint_directory = os.path.dirname(blueprint_path)
         if not blueprint_directory:
-            #blueprint path only contains a file name from the local directory
+            # blueprint path only contains a file name from the local directory
             blueprint_directory = os.getcwd()
         tar_path = '{0}/{1}.tar.gz'.format(tempdir, blueprint_name)
         with tarfile.open(tar_path, "w:gz") as tar:
