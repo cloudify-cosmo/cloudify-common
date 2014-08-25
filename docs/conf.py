227591ac915d26868425aec6aa2a4941736bb0b4
nir0s <nir36g@gmail.com>
2014-08-25 11:32:58 +0300

    fixed 2.6 conflicts

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ copyright = '2014, Gigaspaces'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../{}'.format(package)))
+sys.path.insert(0, os.path.abspath('../{0}'.format(package)))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
