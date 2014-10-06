e2a47670f4106b81f830ecb736b8c90443dcc7ee
opencm <limor@gigaspaces.com>
2014-10-06 21:38:58 +0300

    Bump version to 3.1m4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-rest-client',
-    version='3.1a5',
+    version='3.1a4',
     author='ran',
     author_email='ran@gigaspaces.com',
     packages=['cloudify_rest_client'],
