456e3a51c6dca6fa0d4c2f55b912b0fdea498a1c
Dan Kilman <dankilman@gmail.com>
2014-11-09 16:08:08 +0200

    CFY-1500 rename node_id to node_name in list node instances REST call

--- cloudify_rest_client/node_instances.py
+++ cloudify_rest_client/node_instances.py
@@ -129,14 +129,15 @@ class NodeInstancesClient(object):
         response = self.api.patch(uri, data=data)
         return NodeInstance(response)
 
-    def list(self, deployment_id=None, node_id=None, _include=None):
+    def list(self, deployment_id=None, node_name=None, _include=None):
         """
         Returns a list of node instances which belong to the deployment
         identified by the provided deployment id.
 
-        :param deployment_id: The deployment's id to list node instances for.
-        :param node_id: Optional node id to read only node instances of this
-                        node
+        :param deployment_id: Optional deployment id to list node instances
+                              for.
+        :param node_name: Optional node id to only fetch node instances with
+                          this name
         :param _include: List of fields to include in response.
         :return: Node instances.
         :rtype: list
@@ -144,8 +145,8 @@ class NodeInstancesClient(object):
         params = {}
         if deployment_id:
             params['deployment_id'] = deployment_id
-        if node_id:
-            params['node_id'] = node_id
+        if node_name:
+            params['node_name'] = node_name
         response = self.api.get('/node-instances',
                                 params=params,
                                 _include=_include)
