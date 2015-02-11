dc1af9f4fa04d6ffa986d73eec3175314f1339e5
opencm <limor@gigaspaces.com>
2015-02-11 10:48:46 +0200

    Bump version to 1.2m5

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ from setuptools import setup
 
 setup(
     name='cloudify-script-plugin',
-    version='1.2a4',
+    version='1.2a5',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=['script_runner'],
     description='Plugin for running scripts',
     install_requires=[
-        'cloudify-plugins-common>=3.2a4'
+        'cloudify-plugins-common>=3.2a5'
     ],
     license='LICENSE'
 )
