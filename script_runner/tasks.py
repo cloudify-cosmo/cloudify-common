24ebb676ee4c9f73ede37c951144070c7bcefd8a
nirb <nirb@gigaspaces.com>
2015-10-11 16:14:52 +0300

    CFY-3757 fixed env must contain only strings. Only happened in windows. Converted env keys to strings.

--- script_runner/tasks.py
+++ script_runner/tasks.py
@@ -72,6 +72,7 @@ def create_process_config(process, operation_kwargs):
         del env_vars['ctx']
     env_vars.update(process.get('env', {}))
     for k, v in env_vars.items():
+        k = str(k)
         if isinstance(v, (dict, list, set, bool)):
             env_vars[k] = json.dumps(v)
         else:
