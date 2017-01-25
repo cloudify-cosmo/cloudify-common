80dfed998405acbcfdee3f8c15a6ece54cd69424
opencm <limor@gigaspaces.com>
2017-01-25 15:31:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-rest-client',
-    version='4.0a12',
+    version='4.0a13',
     author='cosmo-admin',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_rest_client'],
