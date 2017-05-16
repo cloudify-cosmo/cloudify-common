f1cb6f6ad121da17132fbf388710d535321ae9d1
Yariv Hamamy <yariv@gigaspaces.com>
2017-05-16 15:15:31 +0300

    CFY-6870 add certificates replacement after snapshot restore

--- cloudify_rest_client/snapshots.py
+++ cloudify_rest_client/snapshots.py
@@ -143,7 +143,8 @@ class SnapshotsClient(object):
                 snapshot_id,
                 recreate_deployments_envs=True,
                 force=False,
-                tenant_name=None):
+                tenant_name=None,
+                restore_certificates=False):
         """
         Restores the snapshot whose id matches the provided snapshot id.
 
@@ -154,13 +155,16 @@ class SnapshotsClient(object):
         actually clean.
         :param tenant_name: Name of the tenant to which old (pre 4.0)
         snapshots should be restored
+        :param restore_certificates: Whether to try and restore the
+        certificates from the snapshot.
         """
         assert snapshot_id
         uri = '/snapshots/{0}/restore'.format(snapshot_id)
         params = {
             'recreate_deployments_envs': recreate_deployments_envs,
             'force': force,
-            'tenant_name': tenant_name
+            'tenant_name': tenant_name,
+            'restore_certificates': restore_certificates
         }
         response = self.api.post(uri, data=params)
         return Execution(response)
