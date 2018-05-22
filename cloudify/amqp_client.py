52d1c68fb8d5e4df2502db309ad53533f3c9d472
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-05-22 19:59:55 +0200

    return self from __enter__
    allows `with get_client() as client`

--- cloudify/amqp_client.py
+++ cloudify/amqp_client.py
@@ -183,6 +183,7 @@ class AMQPConnection(object):
 
     def __enter__(self):
         self.consume_in_thread()
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
