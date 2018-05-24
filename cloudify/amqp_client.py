8caeecc8ed87fb99e2eb108148626a15229b2446
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-05-24 14:05:02 +0200

    Clarify in the docstring

--- cloudify/amqp_client.py
+++ cloudify/amqp_client.py
@@ -119,6 +119,10 @@ class AMQPConnection(object):
         """
         In cases where the broker IP has changed, we want to update the
         environment variables
+
+        This is only relevant when were running as the agent worker,
+        and should have no effect when AMQPConnection is used in other
+        places (eg. cfy-agent).
         """
         if constants.MANAGER_FILE_SERVER_URL_KEY not in os.environ:
             return
