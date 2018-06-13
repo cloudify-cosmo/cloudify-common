02173bedae8204b4faa2c2f0108fe10dad77a9ff
opencm <limor@gigaspaces.com>
2018-06-13 21:48:37 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ except ImportError as e:
 
 setup(
     name='cloudify-common',
-    version='4.4.dev1',
+    version='4.4',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(),
