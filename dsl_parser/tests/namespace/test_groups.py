1c2bb9e0d57a03daf46eb4d420f344793c2fe1a7
tyacbovi <tyacbovi@gmail.com>
2018-11-14 22:32:29 +0200

    Group unit tests - but YAML bug appears
    ALL WORKS

---
+++ dsl_parser/tests/namespace/test_groups.py
@@ -0,0 +1,122 @@
+########
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from dsl_parser import constants
+from dsl_parser.tests.abstract_test_parser import AbstractTestParser
+
+
+class TestNamespacedGroups(AbstractTestParser):
+    def test_groups_definition(self):
+        imported_yaml = self.BASIC_TYPE + """
+node_types:
+    test_type: {}
+node_templates:
+    node1:
+        type: test_type
+groups:
+    group:
+        members: [node1]
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
+
+        main_yaml = self.BASIC_VERSION_SECTION_DSL_1_3 + """
+imports:
+    -   {0}::{1}
+""".format('test', import_file_name)
+        parsed_yaml = self.parse(main_yaml)
+        groups = parsed_yaml[constants.GROUPS]
+        self.assertEqual(groups['test::group']['members'],
+                         ['test::node1'])
+
+    def test_basic_namespace_multi_import(self):
+        imported_yaml = self.BASIC_TYPE + """
+node_types:
+    test_type: {}
+node_templates:
+    node1:
+        type: test_type
+groups:
+    group:
+        members: [node1]
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
+
+        main_yaml = self.BASIC_VERSION_SECTION_DSL_1_3 + """
+imports:
+    -   {0}::{1}
+    -   {2}::{1}
+""".format('test', import_file_name, 'other_test')
+
+        parsed_yaml = self.parse(main_yaml)
+        groups = parsed_yaml[constants.GROUPS]
+        self.assertEqual(2, len(groups))
+        self.assertEqual(groups['test::group']['members'],
+                         ['test::node1'])
+        self.assertEqual(groups['other_test::group']['members'],
+                         ['other_test::node1'])
+
+    def test_group_collision(self):
+        imported_yaml = self.BASIC_TYPE + """
+node_types:
+    test_type: {}
+node_templates:
+    node1:
+        type: test_type
+groups:
+    group:
+        members: [node1]
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
+        main_yaml = """
+imports:
+  - {0}::{1}
+groups:
+    group:
+        members: ["test::node1"]
+""".format('test', import_file_name)
+        parsed_yaml = self.parse_1_3(main_yaml)
+        groups = parsed_yaml[constants.GROUPS]
+        self.assertEqual(2, len(groups))
+        self.assertEqual(groups['test::group']['members'],
+                         ['test::node1'])
+        self.assertEqual(groups['group']['members'],
+                         ['test::node1'])
+
+    def test_imports_merging_with_no_collision(self):
+        imported_yaml = self.BASIC_TYPE + """
+node_types:
+    test_type: {}
+node_templates:
+    node1:
+        type: test_type
+groups:
+    group1:
+        members: [node1]
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
+        main_yaml = """
+imports:
+  - {0}::{1}
+groups:
+    group2:
+        members: ["test::node1"]
+""".format('test', import_file_name)
+        parsed_yaml = self.parse_1_3(main_yaml)
+        groups = parsed_yaml[constants.GROUPS]
+        self.assertEqual(2, len(groups))
+        self.assertEqual(groups['test::group1']['members'],
+                         ['test::node1'])
+        self.assertEqual(groups['group2']['members'],
+                         ['test::node1'])
