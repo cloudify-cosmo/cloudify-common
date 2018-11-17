cb4835166c046433548aa56faac45e008e422417
tyacbovi <tyacbovi@gmail.com>
2018-11-17 22:04:52 +0200

    Update full policies unit tests
    NOT ALL TESTS WORKS - policy fails

--- dsl_parser/tests/namespace/test_policies.py
+++ dsl_parser/tests/namespace/test_policies.py
@@ -13,12 +13,32 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-from dsl_parser import constants
+from dsl_parser import constants, exceptions
 from dsl_parser.tests.abstract_test_parser import AbstractTestParser
 
 
 class TestNamespacedPolicies(AbstractTestParser):
-    def test_basic_policiec(self):
+    def _assert_policy(self,
+                       policy,
+                       targets,
+                       policy_type,
+                       min_instances,
+                       max_instances,
+                       default_instances):
+        self.assertEqual(policy['targets'], targets)
+        self.assertEqual(policy['type'], policy_type)
+
+        policy_properties = policy['properties']
+        self.assertEqual(min_instances, policy_properties['min_instances'])
+        self.assertEqual(max_instances, policy_properties['max_instances'])
+        self.assertEqual(default_instances,
+                         policy_properties['current_instances'])
+        self.assertEqual(default_instances,
+                         policy_properties['planned_instances'])
+        self.assertEqual(default_instances,
+                         policy_properties['default_instances'])
+
+    def test_basic_policies(self):
         imported_yaml = """
 groups:
   group:
@@ -31,6 +51,10 @@ policies:
   policy:
     targets: [group]
     type: cloudify.policies.scaling
+    properties:
+            default_instances: 2
+            min_instances: 1
+            max_instances: 10
 relationships:
   cloudify.relationships.contained_in: {}
 """
@@ -41,17 +65,42 @@ imports:
     -   {0}::{1}
 """.format('test', import_file_name)
         parsed_yaml = self.parse(main_yaml)
-        policies = parsed_yaml[constants.POLICIES]
-        self.assertEqual(policies['test::policy']['targets'],
-                         ['test::group'])
-        self.assertEqual(policies['test::policy']['type'],
-                         'test::cloudify.policies.scaling')
+        policy = parsed_yaml[constants.POLICIES]['test::policy']
+        self._assert_policy(policy,
+                            targets= ['test::group'],
+                            policy_type='test::cloudify.policies.scaling',
+                            min_instances= 1,
+                            max_instances= 10,
+                            default_instances= 2)
 
-    def test_basic_namespace_multi_import(self):
-        pass
+    def test_can_not_merge_namespace_policies(self):
+        imported_yaml = """
+groups:
+  group:
+    members: [node]
+node_templates:
+  node: {type: type}
+node_types:
+  type: {}
+policies:
+  policy:
+    targets: [group]
+    type: cloudify.policies.scaling
+    properties:
+            default_instances: 2
+            min_instances: 1
+            max_instances: 10
+relationships:
+  cloudify.relationships.contained_in: {}
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
 
-    def test_policies_collision(self):
-        pass
-
-    def test_imports_merging_with_no_collision(self):
-        pass
+        main_yaml = self.BASIC_VERSION_SECTION_DSL_1_3 + """
+imports:
+    -   {0}::{1}
+    -   {2}::{1}
+""".format('test', import_file_name, 'other_test')
+        self.assertRaises(
+                exceptions.DSLParsingLogicException,
+                self.parse,
+                main_yaml)
