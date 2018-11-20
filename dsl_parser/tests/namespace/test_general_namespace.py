03a60413ca5b0ae13b09df576d6cb6c92c2f01a2
tyacbovi <tyacbovi@gmail.com>
2018-11-20 11:29:06 +0200

    Adding unique unit tests
    ALL WORKS

---
+++ dsl_parser/tests/namespace/test_general_namespace.py
@@ -0,0 +1,35 @@
+########
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from dsl_parser import exceptions
+from dsl_parser.tests.abstract_test_parser import AbstractTestParser
+
+
+class TestGeneralNamspace(AbstractTestParser):
+    def test_import_with_two_namespace(self):
+        imported_yaml = """
+inputs:
+    port:
+        default: 8080
+"""
+        import_file_name = self.make_yaml_file(imported_yaml)
+
+        main_yaml = self.BASIC_VERSION_SECTION_DSL_1_3 + """
+imports:
+    -   {0}::{0}::{1}
+""".format('test', import_file_name)
+        self.assertRaises(exceptions.DSLParsingLogicException,
+                          self.parse,
+                          main_yaml)
