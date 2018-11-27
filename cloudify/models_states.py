01b54abe896425b78442ecc1a4cbcefab4e0e027
Adi Grabow <adigrabow@gmail.com>
2018-11-27 15:10:46 +0200

    Added new 'scheduled' state to ExecutionState

--- cloudify/models_states.py
+++ cloudify/models_states.py
@@ -44,14 +44,16 @@ class ExecutionState(object):
     FORCE_CANCELLING = 'force_cancelling'
     KILL_CANCELLING = 'kill_cancelling'
     QUEUED = 'queued'
+    SCHEDULED = 'scheduled'
 
     STATES = [TERMINATED, FAILED, CANCELLED, PENDING, STARTED,
-              CANCELLING, FORCE_CANCELLING, KILL_CANCELLING, QUEUED]
+              CANCELLING, FORCE_CANCELLING, KILL_CANCELLING, QUEUED, SCHEDULED]
 
+    SCHEDULED_STATE = [SCHEDULED]
     QUEUED_STATE = [QUEUED]
     END_STATES = [TERMINATED, FAILED, CANCELLED]
     ACTIVE_STATES = [state for state in STATES if state not in END_STATES and
-                     state not in QUEUED_STATE]
+                     state not in QUEUED_STATE not in SCHEDULED_STATE]
 
 
 class VisibilityState(object):
