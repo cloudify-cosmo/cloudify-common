e835fc4163c5f57b9d3f66380add6bdcaec2110d
tyacbovi <tyacbovi@gmail.com>
2019-01-13 11:21:05 +0200

    CY-962: node templates relationships overloading
    - Node templates relationships overloading support
    - General refactors to namespacing process

--- dsl_parser/framework/parser.py
+++ dsl_parser/framework/parser.py
@@ -305,13 +305,18 @@ class Context(object):
                     traverse_list(holder_element.value[holder_key], v)
 
         def should_add_namespace_to_string_leaf(element):
-            return \
-                (isinstance(element._initial_value, basestring) and
-                 element._initial_value not in
-                 constants.USER_PRIMITIVE_TYPES and
-                 not utils.check_if_cloudify_type(element._initial_value) and
-                 not hasattr(element.initial_value_holder,
-                             SKIP_NAMESPACE_FLAG) and
+            if not isinstance(element._initial_value, str):
+                return False
+            is_premitive_type = (element._initial_value in
+                                 constants.USER_PRIMITIVE_TYPES)
+            overridable_cloudify_type = \
+                utils.check_if_overridable_cloudify_type(
+                    element._initial_value)
+            should_skip_adding_namespace =\
+                hasattr(element.initial_value_holder, SKIP_NAMESPACE_FLAG)
+            return (not is_premitive_type and
+                    not overridable_cloudify_type and
+                    not should_skip_adding_namespace and
                     element.add_namespace_to_schema_elements)
 
         def set_leaf_namespace(element):
@@ -336,7 +341,9 @@ class Context(object):
         def should_add_element_namespace(element_holder):
             # Preventing of adding namespace prefix to cloudify
             # basic types.
-            return (not utils.check_if_cloudify_type(element_holder.value) and
+            overridable_cloudify_type =\
+                utils.check_if_overridable_cloudify_type(element_holder.value)
+            return (not overridable_cloudify_type and
                     not hasattr(element_holder, SKIP_NAMESPACE_FLAG))
 
         def set_element_namespace(element_namespace, element_holder):
--- dsl_parser/tests/abstract_test_parser.py
+++ dsl_parser/tests/abstract_test_parser.py
@@ -188,21 +188,25 @@ imports:"""
                          resolver=resolver,
                          validate_version=validate_version)
 
-    def parse_1_0(self, dsl_string, resources_base_path=None):
+    def parse_1_0(self, dsl_string, resources_base_path=None, resolver=None):
         return self.parse(dsl_string, resources_base_path,
-                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_0)
+                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_0,
+                          resolver=resolver)
 
-    def parse_1_1(self, dsl_string, resources_base_path=None):
+    def parse_1_1(self, dsl_string, resources_base_path=None, resolver=None):
         return self.parse(dsl_string, resources_base_path,
-                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_1)
+                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_1,
+                          resolver=resolver)
 
-    def parse_1_2(self, dsl_string, resources_base_path=None):
+    def parse_1_2(self, dsl_string, resources_base_path=None, resolver=None):
         return self.parse(dsl_string, resources_base_path,
-                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_2)
+                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_2,
+                          resolver=resolver)
 
-    def parse_1_3(self, dsl_string, resources_base_path=None):
+    def parse_1_3(self, dsl_string, resources_base_path=None, resolver=None):
         return self.parse(dsl_string, resources_base_path,
-                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_3)
+                          dsl_version=self.BASIC_VERSION_SECTION_DSL_1_3,
+                          resolver=resolver)
 
     def parse_from_path(self, dsl_path, resources_base_path=None):
         return dsl_parse_from_path(dsl_path, resources_base_path)
