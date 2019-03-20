2e83ba27683512fa5bf1b7e00de93507b72a8825
David Ginzbourg <davidginzbourg@gmail.com>
2019-03-20 12:29:02 +0200

    CY-1060 Add plugin filtering and merging logic

--- cloudify/tests/test_utils.py
+++ cloudify/tests/test_utils.py
@@ -19,12 +19,15 @@ import logging
 import tempfile
 import unittest
 
+from cloudify import utils
 from cloudify.exceptions import CommandExecutionException
 from cloudify.utils import (
     setup_logger,
+    merge_plugins,
     get_exec_tempdir,
-    LocalCommandRunner
-)
+    LocalCommandRunner)
+
+from dsl_parser.constants import PLUGIN_INSTALL_KEY, PLUGIN_NAME_KEY
 
 
 class LocalCommandRunnerTest(unittest.TestCase):
@@ -65,3 +68,91 @@ class TempdirTest(unittest.TestCase):
     @mock.patch.dict(os.environ, {'CFY_EXEC_TEMP': '/fake/temp'})
     def test_executable_override(self):
         self.assertEqual('/fake/temp', get_exec_tempdir())
+
+
+class TestPluginFunctions(unittest.TestCase):
+
+    def test_extract_plugins_is_correct(self):
+        def true(_):
+            return True
+
+        def false(_):
+            return False
+
+        def three(p):
+            return p['dummy'] == 3
+
+        plugin_list = [{PLUGIN_INSTALL_KEY: True, 'dummy': 1}] * 2
+        result = utils.extract_plugins_to_install(plugin_list, true)
+        self.assertListEqual(result, plugin_list)
+        result = utils.extract_plugins_to_install(plugin_list, false)
+        self.assertListEqual(result, [])
+
+        p = {PLUGIN_INSTALL_KEY: True, 'dummy': 3}
+        plugin_list.append(p)
+        result = utils.extract_plugins_to_install(plugin_list, three)
+        self.assertListEqual(result, [p])
+
+    def test_extract_and_merge_to_install_executes_correctly(self):
+        with mock.patch('cloudify.utils.merge_plugins') as merge_plugins_mock:
+            dep_plugins = [{PLUGIN_INSTALL_KEY: True, 'dummy': 1},
+                           {PLUGIN_INSTALL_KEY: True, 'dummy': 2}]
+            wf_plugins = [{PLUGIN_INSTALL_KEY: True, 'dummy': 2}]
+            utils.extract_and_merge_plugins_to_install(
+                dep_plugins, wf_plugins, lambda x: x['dummy'] == 2)
+            merge_plugins_mock.assert_called_with(dep_plugins[1:], wf_plugins)
+
+    def test_merge_is_correct(self):
+        deployment_plugins = [{PLUGIN_NAME_KEY: 'dummy{}'.format(i)}
+                              for i in range(3)]
+        workflow_plugins = deployment_plugins + [{PLUGIN_NAME_KEY: 'dummy3'}]
+        result = merge_plugins(deployment_plugins, workflow_plugins)
+        # Checks that no duplicates are made
+        self.assertListEqual(result, workflow_plugins)
+
+    def test_extract_and_merge_to_uninstall_executes_correctly(self):
+        dep_plugins = [{PLUGIN_INSTALL_KEY: True, 'dummy': 1},
+                       {PLUGIN_INSTALL_KEY: True, 'dummy': 2}]
+        wf_plugins = [{PLUGIN_INSTALL_KEY: True, 'dummy': 2}]
+        res = utils.extract_and_merge_plugins_to_uninstall(
+            dep_plugins, wf_plugins, lambda x: x['dummy'] == 2)
+        self.assertListEqual(dep_plugins[1:] + wf_plugins, res)
+
+    def test_install_task_not_executed(self):
+        utils.add_plugins_to_install(None, False, None)
+
+    def test_uinstall_task_not_executed(self):
+        utils.add_plugins_to_uninstall(None, False, None)
+
+
+class TestPluginInstallationTaskExecutionFuncs(unittest.TestCase):
+    def setUp(self):
+        super(TestPluginInstallationTaskExecutionFuncs, self).setUp()
+        self.ctx_mock = mock.Mock()
+        self.ctx_mock.send_event.return_value = 'send_event'
+        self.ctx_mock.execute_task.return_value = 'execute_task'
+        self.sequence_mock = mock.Mock()
+
+    def test_add_plugin_to_install_adds_to_sequence(self):
+        plugins_list = ['something']
+        utils.add_plugins_to_install(
+            self.ctx_mock, plugins_list, self.sequence_mock)
+
+        self.ctx_mock.send_event.assert_called()
+        self.ctx_mock.execute_task.assert_called_with(
+            task_name='cloudify_agent.operations.install_plugins',
+            kwargs={'plugins': plugins_list})
+        self.sequence_mock.add.assert_called_with('send_event', 'execute_task')
+
+    def test_add_plugin_to_uninstall_adds_to_sequence(self):
+        plugins_list = ['something']
+        utils.add_plugins_to_uninstall(
+            self.ctx_mock, plugins_list, self.sequence_mock)
+
+        self.ctx_mock.send_event.assert_called()
+        self.ctx_mock.execute_task.assert_called_with(
+            task_name='cloudify_agent.operations.uninstall_plugins',
+            kwargs={
+                'plugins': plugins_list,
+                'delete_managed_plugins': False})
+        self.sequence_mock.add.assert_called_with('send_event', 'execute_task')
