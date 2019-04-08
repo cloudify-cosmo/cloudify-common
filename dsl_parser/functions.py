61aefeda57964301a2a21f1fdf822ed47df17d35
Jonathan <abramso@gmail.com>
2019-04-08 19:55:25 +0300

    Update functions.py (#260)
    Recursion limit was increased from 10 to 50 to avoid breaking multi layers blueprints

--- dsl_parser/functions.py
+++ dsl_parser/functions.py
@@ -879,7 +879,7 @@ def _handler(evaluator, **evaluator_kwargs):
             return msg.format(kwargs['path'])
         return ""
 
-    @limit_recursion(10, args_to_str_func=_args_to_str_func)
+    @limit_recursion(50, args_to_str_func=_args_to_str_func)
     def handler(v, scope, context, path):
         evaluated_value = v
         scanned = False
