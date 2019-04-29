aa511c7fc2b7d23d943175e68619e08d068601f5
asdf <tehasdf@gmail.com>
2019-04-29 06:58:40 +0200

    Move tasks impl to tasks.py

--- cloudify/workflows/tasks.py
+++ cloudify/workflows/tasks.py
@@ -18,14 +18,19 @@ import time
 import uuid
 import Queue
 
-from cloudify import exceptions
+from cloudify import exceptions, logs
 from cloudify.workflows import api
-from cloudify.manager import get_rest_client
+from cloudify.manager import (
+    get_rest_client,
+    get_node_instance,
+    update_execution_status,
+    update_node_instance
+)
 from cloudify.constants import MGMTWORKER_QUEUE
+from cloudify.state import current_workflow_ctx
 from cloudify.utils import exception_to_error_cause
 # imported for backwards compat:
 from cloudify.utils import INSPECT_TIMEOUT  # noqa
-
 
 INFINITE_TOTAL_RETRIES = -1
 DEFAULT_TOTAL_RETRIES = INFINITE_TOTAL_RETRIES
@@ -953,3 +958,172 @@ class HandlerResult(object):
     @classmethod
     def ignore(cls):
         return HandlerResult(cls.HANDLER_IGNORE)
+
+
+# Local tasks implementation
+# A local task is a callable that will be passed to a LocalWorkflowTask,
+# and then executed in a separate thread (see LocalTasksProcessing). Those
+# tasks can implement .restore (classmethod) and .dump methods in order to
+# be resumable.
+# The user-facing interface for those tasks are node_instance.set_state,
+# .send_event, etc.
+
+class _LocalTask(object):
+    workflow_task_config = {'send_task_events': False}
+
+    _subclass_cache = None
+
+    @classmethod
+    def restore(cls, task_descr):
+        if cls._subclass_cache is None:
+            cls._subclass_cache = {
+                subcls.__name__: subcls for subcls in cls.__subclasses__()
+            }
+        task_class = cls._subclass_cache[task_descr['task']]
+        return task_class(**task_descr['kwargs'])
+
+    def __call__(self):
+        if current_workflow_ctx.local:
+            return self.local()
+        else:
+            return self.remote()
+
+    def local(self):
+        raise NotImplementedError('Implemented by subclasses')
+
+    def remote(self):
+        raise NotImplementedError('Implemented by subclasses')
+
+    @property
+    def storage(self):
+        return current_workflow_ctx.internal.handler.storage
+
+
+class _SetNodeInstanceStateTask(_LocalTask):
+    def __init__(self, node_instance_id, state):
+        self._node_instance_id = node_instance_id
+        self._state = state
+
+    def dump(self):
+        return {
+            'task': self.__class__.__name__,
+            'kwargs': {
+                'node_instance_id': self._node_instance_id,
+                'state': self._state
+            }
+        }
+
+    def remote(self):
+        node_instance = get_node_instance(self._node_instance_id)
+        node_instance.state = self._state
+        update_node_instance(node_instance)
+        return node_instance
+
+    def local(self):
+        current_workflow_ctx.storage.update_node_instance(
+            self._node_instance_id,
+            state=self._state,
+            version=None)
+
+
+class _GetNodeInstanceStateTask(_LocalTask):
+    def __init__(self, node_instance_id):
+        self._node_instance_id = node_instance_id
+
+    def dump(self):
+        return {
+            'task': self.__class__.__name__,
+            'kwargs': {
+                'node_instance_id': self._node_instance_id
+            }
+        }
+
+    def remote(self):
+        return get_node_instance(self._node_instance_idd).state
+
+    def local(self):
+        instance = current_workflow_ctx.storage.get_node_instance(
+            self._node_instance_id)
+        return instance.state
+
+
+class _SendNodeEventTask(_LocalTask):
+    def __init__(self, node_instance_id, event, additional_context):
+        self._node_instance_id = node_instance_id
+        self._event = event
+        self._additional_context = additional_context
+
+    def dump(self):
+        return {
+            'task': self.__class__.__name__,
+            'kwargs': {
+                'node_instance_id': self._node_instance_id,
+                'event': self._event,
+                'additional_context': self._additional_context
+            }
+        }
+
+    def remote(self):
+        self.send(out_func=logs.logs.amqp_event_out)
+
+    def local(self):
+        self.send(out_func=logs.stdout_event_out)
+
+    def send(self, out_func):
+        node_instance = current_workflow_ctx.get_node_instance(
+            self._node_instance_id)
+        logs.send_workflow_node_event(
+            ctx=node_instance,
+            event_type='workflow_node_event',
+            message=self._event,
+            additional_context=self._additional_context,
+            out_func=out_func)
+
+
+class _SendWorkflowEventTask(_LocalTask):
+    def __init__(self, event, event_type, args, additional_context=None):
+        self._event = event
+        self._event_type = event_type
+        self._args = args
+        self._additional_context = additional_context
+
+    def dump(self):
+        return {
+            'task': self.__class__.__name__,
+            'kwargs': {
+                'event': self._event,
+                'event_type': self.event_type,
+                'args': self._args,
+                'additional_context': self._additional_context
+            }
+        }
+
+    def __call__(self):
+        return current_workflow_ctx.internal.send_workflow_event(
+            event_type=self._event_type,
+            message=self._event,
+            args=self._args,
+            additional_context=self._additional_context
+        )
+
+
+class _UpdateExecutionStatusTask(_LocalTask):
+    def __init__(self, status):
+        self._status = status
+
+    def dump(self):
+        return {
+            'task': self.__class__.__name__,
+            'kwargs': {
+                'status': self._status,
+            }
+        }
+
+    def remote(self):
+        update_execution_status(
+            current_workflow_ctx.execution_id, self._status)
+
+    def local(self, new_status):
+        raise NotImplementedError(
+            'Update execution status is not supported for '
+            'local workflow execution')
