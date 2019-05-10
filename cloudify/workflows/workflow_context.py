38a812f4365350115067a7c2ae98bfa52e4289ad
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2019-05-10 14:22:07 +0200

    Scale up: refresh node instances (#290)
    After scaling up, node instances must be refreshed so that
    workflow_ctx.get_node_instances(...) can return the new ones
    created in the scale

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -923,6 +923,21 @@ class WorkflowNodesAndInstancesContainer(object):
         """
         return self._node_instances.get(node_instance_id)
 
+    def refresh_node_instances(self):
+        if self.local:
+            storage = self.internal.handler.storage
+            raw_node_instances = storage.get_node_instances()
+        else:
+            rest = get_rest_client()
+            raw_node_instances = rest.node_instances.list(
+                deployment_id=self.deployment.id,
+                _get_all_results=True)
+        self._node_instances = dict(
+            (instance.id, CloudifyWorkflowNodeInstance(
+                self, self._nodes[instance.node_id], instance,
+                self))
+            for instance in raw_node_instances)
+
 
 class CloudifyWorkflowContext(
     _WorkflowContextBase,
