b93ecc411732a900dab26a15d564b87c721db340
asdf <tehasdf@gmail.com>
2019-05-24 17:01:30 +0200

    Update broker_config: drop unused items, allow reloading

--- cloudify/broker_config.py
+++ cloudify/broker_config.py
@@ -14,7 +14,6 @@
 #    * limitations under the License.
 
 # AMQP broker configuration for agents and manager
-# Primarily used by celery, so provided with variables it understands
 from __future__ import absolute_import
 
 import json
@@ -23,67 +22,66 @@ import ssl
 
 from cloudify.constants import BROKER_PORT_SSL, BROKER_PORT_NO_SSL
 
-workdir_path = os.getenv('AGENT_WORK_DIR')
-if workdir_path is None:
-    # We are not in an appropriately configured celery environment
-    config = {}
-else:
-    conf_file_path = os.path.join(workdir_path, 'broker_config.json')
-    if os.path.isfile(conf_file_path):
-        with open(conf_file_path) as conf_handle:
-            config = json.load(conf_handle)
+
+def get_config_path():
+    workdir_path = os.getenv('AGENT_WORK_DIR')
+    if workdir_path is None:
+        # We are not in an appropriately configured worker environment
+        return None
     else:
-        config = {}
+        return os.path.join(workdir_path, 'broker_config.json')
+
+
+def load_broker_config(path=None):
+    """Load all the config variables from the config file.
+
+    Users of this module expect the config to be exposed as global
+    variables, so we set them. This allows to re-load them if needed.
+
+    :param path: Path to the config file.
+                 (default: $AGENT_WORK_DIR/broker_config.json)
+    """
+    global broker_cert_path, broker_username, broker_password, broker_vhost, \
+        broker_hostname, broker_ssl_enabled, broker_port, \
+        broker_management_hostname, broker_heartbeat, broker_ssl_options
+
+    path = path or get_config_path()
+    if not path or not os.path.isfile(path):
+        return
+    with open(path) as f:
+        config = json.load(f)
+
+    broker_cert_path = config.get('broker_cert_path') or broker_cert_path
+    broker_username = config.get('broker_username') or broker_username
+    broker_password = config.get('broker_password') or broker_password
+    broker_hostname = config.get('broker_hostname') or broker_hostname
+    broker_vhost = config.get('broker_vhost') or broker_vhost
+    broker_ssl_enabled = config.get('broker_ssl_enabled') or broker_ssl_enabled
+    broker_port = BROKER_PORT_SSL if broker_ssl_enabled else BROKER_PORT_NO_SSL
+    broker_management_hostname = \
+        config.get('broker_management_hostname') or broker_management_hostname
+
+    broker_heartbeat = config.get('broker_heartbeat') or broker_heartbeat
+    if broker_ssl_enabled:
+        broker_ssl_options = {
+            'ca_certs': broker_cert_path,
+            'cert_reqs': ssl.CERT_REQUIRED,
+        }
+    else:
+        broker_ssl_options = {}
+
 
 # Provided as variables for retrieval by amqp_client and logger as required
-broker_cert_path = config.get('broker_cert_path', '')
-broker_username = config.get('broker_username', 'guest')
-broker_password = config.get('broker_password', 'guest')
-broker_hostname = config.get('broker_hostname', 'localhost')
-broker_vhost = config.get('broker_vhost', '/')
-broker_ssl_enabled = config.get('broker_ssl_enabled', False)
-broker_port = BROKER_PORT_SSL if broker_ssl_enabled else BROKER_PORT_NO_SSL
-broker_management_hostname = config.get('broker_management_hostname',
-                                        'localhost')
-
-# only enable heartbeat by default for agents connected to a cluster
-DEFAULT_HEARTBEAT = 30
-if os.name == 'nt':
-    # celery doesn't support broker_heartbeat on windows
-    broker_heartbeat = None
-else:
-    broker_heartbeat = config.get('broker_heartbeat', DEFAULT_HEARTBEAT)
-
+broker_cert_path = ''
+broker_username = 'guest'
+broker_password = 'guest'
+broker_hostname = 'localhost'
+broker_vhost = '/'
+broker_ssl_enabled = False
+broker_port = BROKER_PORT_NO_SSL
+broker_management_hostname = 'localhost'
 broker_ssl_options = {}
-if broker_ssl_enabled:
-    broker_ssl_options = {
-        'ca_certs': broker_cert_path,
-        'cert_reqs': ssl.CERT_REQUIRED,
-    }
+broker_heartbeat = 30
 
-if broker_heartbeat:
-    options = '?heartbeat={heartbeat}'.format(heartbeat=broker_heartbeat)
-else:
-    options = ''
-
-# BROKER_URL is held in the config to avoid the password appearing
-# in ps listings
-URL_TEMPLATE = \
-    'amqp://{username}:{password}@{hostname}:{port}/{vhost}{options}'
-if config.get('cluster'):
-    BROKER_URL = ';'.join(URL_TEMPLATE.format(username=broker_username,
-                                              password=broker_password,
-                                              hostname=node_ip,
-                                              port=broker_port,
-                                              vhost=broker_vhost,
-                                              options=options)
-                          for node_ip in config['cluster'])
-else:
-    BROKER_URL = URL_TEMPLATE.format(
-        username=broker_username,
-        password=broker_password,
-        hostname=broker_hostname,
-        port=broker_port,
-        vhost=broker_vhost,
-        options=options
-    )
+# defaults are overridden from file at import time
+load_broker_config()
