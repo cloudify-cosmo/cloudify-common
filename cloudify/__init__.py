f135c2988495279c503596ddf48ecbfaa2da1cff
asdf <tehasdf@gmail.com>
2019-05-25 18:01:23 +0200

    Make broker_config an object instead of a module

--- cloudify/__init__.py
+++ cloudify/__init__.py
@@ -13,5 +13,72 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+import json
+import os
+import ssl
+
+from cloudify.constants import BROKER_PORT_SSL, BROKER_PORT_NO_SSL
+
 
 from cloudify.state import ctx  # noqa
+
+_BROKER_CONFIG_DEFAULTS = {
+    'broker_cert_path': '',
+    'broker_username': 'guest',
+    'broker_password': 'guest',
+    'broker_hostname': 'localhost',
+    'broker_vhost': '/',
+    'broker_ssl_enabled': False,
+    'broker_management_hostname': 'localhost',
+    'broker_heartbeat': 30,
+}
+
+
+class _BrokerConfig(object):
+    def __init__(self):
+        for name, value in _BROKER_CONFIG_DEFAULTS.items():
+            setattr(self, name, value)
+
+    def get_config_path(self):
+        workdir_path = os.getenv('AGENT_WORK_DIR')
+        if workdir_path is None:
+            # We are not in an appropriately configured worker environment
+            return None
+        else:
+            return os.path.join(workdir_path, 'broker_config.json')
+
+    def load_broker_config(self, path=None):
+        """Load all the config variables from the config file.
+
+        :param path: Path to the config file.
+                     (default: $AGENT_WORK_DIR/broker_config.json)
+        """
+        path = path or self.get_config_path()
+        if not path or not os.path.isfile(path):
+            return
+        with open(path) as f:
+            config = json.load(f)
+
+        for name in _BROKER_CONFIG_DEFAULTS:
+            value = config.get(name)
+            if value:
+                setattr(self, name, value)
+
+    @property
+    def broker_port(self):
+        return BROKER_PORT_SSL if self.broker_ssl_enabled \
+            else BROKER_PORT_NO_SSL
+
+    @property
+    def broker_ssl_options(self):
+        if self.broker_ssl_enabled:
+            return {
+                'ca_certs': self.broker_cert_path,
+                'cert_reqs': ssl.CERT_REQUIRED,
+            }
+        else:
+            return {}
+
+
+broker_config = _BrokerConfig()
+broker_config.load_broker_config()
