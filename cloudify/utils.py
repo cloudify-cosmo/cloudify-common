0105f72efb999fb6f99966530c6e8d6c6fa0cdda
asdf <tehasdf@gmail.com>
2019-06-14 02:44:13 +0200

    BlockingRequestResponseHandler call: arg name
    We removed __init__ from _RequestResponseHandlerBase, so the param
    names are now from TaskConsumer, and for that, the arg name is
    `queue`. Even though it's really an exchange name, not a queue.
    
    I decided that this is bullshit, and the safe thing to do is to just
    decide that the exchange name shall be passed positionally, not by
    name.
    And in the future we'll also rename the arg.
    
    This means all uses of _RequestResponseHandlerBase's subclasses
    must be audited, and at least a cloudify-agent PR must accompany this.
    
    History: we _did_ know the difference between a rabbitmq exchange
    and a queue when we wrote this, but celery mostly didn't (because
    celery is somewhat-broker-agnostic and the primary user-facing thing
    in celery is a "queue"). And when we were implementing this, we
    inherited most of celery's naming, because of all the envvars etc
    we had around.

--- cloudify/utils.py
+++ cloudify/utils.py
@@ -573,7 +573,7 @@ def is_agent_alive(name,
     :param connect: whether to connect the client (should be False if it is
                     already connected)
     """
-    handler = BlockingRequestResponseHandler(exchange=name)
+    handler = BlockingRequestResponseHandler(name)
     client.add_handler(handler)
     if connect:
         with client:
