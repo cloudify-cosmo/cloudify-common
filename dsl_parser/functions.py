a42859c2c8fbf05da5a3b680243e1961e67cf5cb
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2019-07-30 14:18:46 +0300

    CY-1558: removing any restriction on concat (#346)
    - The missing scope was in capabilities, but there is not real reason to restrict for all places

--- dsl_parser/functions.py
+++ dsl_parser/functions.py
@@ -682,12 +682,6 @@ class Concat(Function):
                 'Using {0} requires using dsl version 1_1 or '
                 'greater, but found: {1} in {2}.'
                 .format(self.name, plan.version, self.path))
-        if self.scope not in [scan.NODE_TEMPLATE_SCOPE,
-                              scan.NODE_TEMPLATE_RELATIONSHIP_SCOPE,
-                              scan.OUTPUTS_SCOPE]:
-            raise ValueError('{0} cannot be used in {1}.'
-                             .format(self.name,
-                                     self.path))
 
     def evaluate(self, plan):
         for joined_value in self.joined:
