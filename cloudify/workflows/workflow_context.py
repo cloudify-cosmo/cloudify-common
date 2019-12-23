88dd5fd725aae653567686e488c8287f2fac89eb
Inbal Amrani <inbala@gigaspaces.com>
2019-12-23 17:32:36 +0200

    CY-2123 Add execution token to the operation context (#387)

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -1395,7 +1395,8 @@ class RemoteContextHandler(CloudifyWorkflowContextHandler):
     def operation_cloudify_context(self):
         return {'local': False,
                 'bypass_maintenance': utils.get_is_bypass_maintenance(),
-                'rest_token': utils.get_rest_token()}
+                'rest_token': utils.get_rest_token(),
+                'execution_token': utils.get_execution_token()}
 
     def download_deployment_resource(self,
                                      blueprint_id,
