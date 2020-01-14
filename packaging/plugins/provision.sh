c1b25b5226696cd550ca69dad0a013440066e14f
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-01-14 11:55:45 +0200

    CY-1834 Refactor provision.sh and Vagrantfile

--- packaging/plugins/provision.sh
+++ packaging/plugins/provision.sh
@@ -14,25 +14,73 @@ function print_plugins_params() {
     done
 }
 
-function wagon_create_package(){
+function install_windows_dependencies(){
+    # Prepare virtualenv in windows machine
+    echo "## Installing necessary dependencies"
+    pip install virtualenv
 
-    echo "## wagon create package"
+    # Prepare pip & wagon in windows machine
+    echo "## installing wagon"
+    virtualenv env
+    source env/bin/activate
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    pip install --upgrade pip==9.0.1 setuptools
+    pip install wagon==0.3.2
+}
+
+function checkout_plugin() {
     echo "git clone https://github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME.git"
     git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME.git
     pushd $PLUGIN_NAME
-        PLUGIN_PATH=$(PWD)
+        PLUGIN_PATH=$(pwd)
         if [ "$PLUGIN_TAG_NAME" == "master" ];then
             git checkout master
         else
             git checkout -b $PLUGIN_TAG_NAME origin/$PLUGIN_TAG_NAME
         fi
     popd
+
+}
+
+function create_windows_wagon_package() {
+    echo "## Generate Wagon Package on Windows"
+    # Checkout to the plugin
+    checkout_plugin
+
+    echo "manylinux1_compatible = False" > "env/bin/_manylinux.py"
+    mkdir create_wagon ; cd create_wagon
+    if [ ! -z "$CONSTRAINTS_FILE" ] && [ -f "/packaging/$CONSTRAINTS_FILE" ];then
+        echo "## /packaging/$CONSTRAINTS_FILE exist"
+        wagon create -s ../$PLUGIN_NAME/ -r -v -f -a '--no-cache-dir -c /packaging/'$CONSTRAINTS_FILE''
+    else
+        echo "## /packaging/$CONSTRAINTS_FILE doesn't exist"
+        wagon create -s ../$PLUGIN_NAME/ -r -v -f
+    fi
+
+}
+
+function generate_windows_plugin() {
+    # Print Plugin params
+    print_plugins_params &&
+    # Install common prerequisite
+    install_common_prereqs &&
+    # Install windows dependencies
+    install_windows_dependencies &&
+    # create_window_wagon
+    create_windows_wagon_package
+}
+
+function wagon_create_package(){
+
+    echo "## wagon create package"
+    # Checkout to the plugin
+    checkout_plugin
     # This will generate a wagon file and dump it to the current plugin name
     # directory, this should work for all linux image but for Redhat we need
     # to build it locally since it needs subscription account
     echo "## echo build wagon package using docker"
-    echo "git clone https://github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME.git"
-    git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_ORGANIZATION/$WAGON_BUILDLER_REPO.git
+    echo "clone https://@github.com/$GITHUB_ORGANIZATION/$WAGON_BUILDLER_REPO.git"
+    git clone https://@github.com/$GITHUB_ORGANIZATION/$WAGON_BUILDLER_REPO.git
     IMAGE_NAME="cloudify-$PLUGIN_PLATFORM-wagon-builder"
     pushd $WAGON_BUILDLER_REPO
          git checkout $WAGON_BUILDLER_BRANCH
@@ -40,7 +88,7 @@ function wagon_create_package(){
              if [$PLUGIN_PLATFORM == redhat*]; then
                   docker build -t $IMAGE_NAME --build-arg USERNAME=$REL_SUB_USERNAME --build-arg PASSWORD=$REL_SUB_PASSWORD .
              else
-                 docker build -t $IMAGE_NAME .
+                  docker build -t $IMAGE_NAME .
              fi
          popd
     popd
@@ -49,7 +97,11 @@ function wagon_create_package(){
     docker run -v $PLUGIN_PATH:/packaging $IMAGE_NAME
 }
 
-
+function override_constraints_file() {
+    if [! -z "$CONSTRAINTS_FILE"];then
+        cp -rf $CONSTRAINTS_FILE $PLUGIN_NAME/
+    if
+}
 
 # VERSION/PRERELEASE/BUILD must be exported as they is being read as an env var by the cloudify-agent-packager
 export CORE_TAG_NAME="5.1.0.dev1"
@@ -67,26 +119,33 @@ PLUGIN_NAME=$5
 PLUGIN_TAG_NAME=$6
 PLUGIN_S3_FOLDER=$7
 GITHUB_ORGANIZATION=$8
+CONSTRAINTS_FILE=$9
 
-if [ "$#" -eq 9 ]; then
-  CONSTRAINTS_FILE=$9
-  echo "There are 9 params"
+DOCKER_BUILDER=false
 
-else
-  echo "There are 13 params"
-  PLUGIN_PLATFORM=$9
-  WAGON_BUILDLER_REPO=${10}
-  WAGON_BUILDLER_BRANCH=${11}
-  REL_SUB_USERNAME=${12}
-  REL_SUB_PASSWORD=${13}
+if [ "$#" -gt 9 ]; then
+    PLUGIN_PLATFORM=${10}
+    WAGON_BUILDLER_REPO=${11}
+    WAGON_BUILDLER_BRANCH=${12}
+    REL_SUB_USERNAME=${13}
+    REL_SUB_PASSWORD=${14}
+    DOCKER_BUILDER=true
 fi
 
 export AWS_S3_BUCKET="cloudify-release-eu"
 export AWS_S3_PATH="cloudify/wagons/$PLUGIN_NAME/$PLUGIN_S3_FOLDER"
 
-print_plugins_params
-wagon_create_package &&
-cd $PLUGIN_NAME &&
-create_md5 "wgn" &&
-[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "wgn" && upload_to_s3 "md5"
+if $DOCKER_BUILDER; then
+  print_plugins_params &&
+  override_constraints_file &&
+  wagon_create_package &&
+  cd $PLUGIN_NAME &&
+  create_md5 "wgn" &&
+  [ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "wgn" && upload_to_s3 "md5"
+else
+   generate_windows_plugin &&
+   cd $PLUGIN_NAME &&
+   create_md5 "wgn" &&
+   [ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "wgn" && upload_to_s3 "md5"
+fi
 
