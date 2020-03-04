1f040be5812419f5d9b4e33c58b864ebf7b7f3a9
asdf <tehasdf@gmail.com>
2020-03-04 15:05:04 +0100

    Events: don't encode the message
    Inside of events, the message is interpolated into a native string,
    so there's no need to encode it.
    
    Later, it is sent via rabbitmq, as json, ie. unicode, so encoding it
    to bytes makes no sense at all.
    
    Without this change, on py3, we see logs with the repr'd `b''` prefix

--- cloudify/event.py
+++ cloudify/event.py
@@ -75,7 +75,7 @@ class Event(object):
 
     @property
     def text(self):
-        message = self._event['message']['text'].encode('utf-8')
+        message = self._event['message']['text']
         if self.is_log_message:
             message = '{0}: {1}'.format(self.log_level, message)
         elif (self.event_type in ('task_rescheduled', 'task_failed')):
