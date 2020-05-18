03998ad2d0fbf5b69c2c025957337b2bcd771157
Yoni Itzhak <yoni@cloudify.co>
2020-05-18 16:59:08 +0300

    removing runtime only evaluation

--- cloudify_rest_client/inter_deployment_dependencies.py
+++ cloudify_rest_client/inter_deployment_dependencies.py
@@ -122,8 +122,7 @@ class InterDeploymentDependencyClient(object):
                                 params=params)
         return self._wrap_list(response)
 
-    def restore(self, deployment_id, update_service_composition,
-                runtime_only_evaluation=False):
+    def restore(self, deployment_id, update_service_composition):
         """
         Updating the inter deployment dependencies table from the specified
         deployment during an upgrade
@@ -132,7 +131,6 @@ class InterDeploymentDependencyClient(object):
         data = {
             'deployment_id': deployment_id,
             'update_service_composition': update_service_composition,
-            'runtime_only_evaluation': runtime_only_evaluation,
         }
         self.api.post('/{self._uri_prefix}/restore'.format(self=self),
                       data=data)
