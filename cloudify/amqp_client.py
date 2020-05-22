19dafd95d4c5e455995c42f0e0f159cb93be7061
Mateusz Neumann <mateumann@gmail.com>
2020-05-22 08:59:35 +0200

    CY-2698 Allow Unicode in Cloudify logs
    Both database (amqp) and console logs can now handle Unicode messages.

--- cloudify/amqp_client.py
+++ cloudify/amqp_client.py
@@ -467,7 +467,7 @@ class SendHandler(object):
         log_func = getattr(self.logger, level, self.logger.info)
         exec_id = message.get('context', {}).get('execution_id')
         text = message['message']['text']
-        msg = '[{0}] {1}'.format(exec_id, text) if exec_id else text
+        msg = u'[{0}] {1}'.format(exec_id, text) if exec_id else text
         log_func(msg)
 
     def publish(self, message, **kwargs):
--- cloudify/logs.py
+++ cloudify/logs.py
@@ -26,6 +26,7 @@ from cloudify import amqp_client_utils
 from cloudify import event as _event
 from cloudify.utils import is_management_environment
 from cloudify.exceptions import ClosedAMQPClientException
+text_type = unicode
 
 EVENT_CLASS = _event.Event
 EVENT_VERBOSITY_LEVEL = _event.NO_VERBOSE
@@ -325,7 +326,7 @@ def create_event_message_prefix(event):
     event_obj = EVENT_CLASS(event, verbosity_level=EVENT_VERBOSITY_LEVEL)
     if not event_obj.has_output:
         return None
-    return str(event_obj)
+    return text_type(event_obj)
 
 
 def with_amqp_client(func):
