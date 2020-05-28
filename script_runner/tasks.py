b004c7765999914b9e58cdb765f57fa2b2bc16be
Mateusz Neumann <mateumann@gmail.com>
2020-05-28 07:07:30 +0000

    CY-2698 Unicode consistency among evns
    Reaching point of consistency between Cloudify environments
    (cfy/mgmtworker/manager).

--- script_runner/tasks.py
+++ script_runner/tasks.py
@@ -414,7 +414,8 @@ class OutputConsumer(object):
 
     def consume_output(self):
         for line in self.out:
-            self.logger.info("%s%s", self.prefix, line.rstrip('\n'))
+            line = line.rstrip('\n').decode('utf-8', 'replace')
+            self.logger.info("%s%s", self.prefix, line)
         self.out.close()
 
     def join(self):
