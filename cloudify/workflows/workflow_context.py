7fa53b0e9d0ee0036ccd818bf65c134804d9ac10
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2020-06-01 10:10:49 +0200

    CY-2900 Refresh node instances (#484)
    * CY-2900 start deployment modification: refresh node instances
    
    In scale-like workflows (which can be custom workflows written
    by the user!), it is always required to refresh node instances
    after starting the scale, so that the local process gets the
    newly-created node instances.
    
    Without refreshing, it doesn't have them, and so it can't install
    them.
    
    Since it is always required, let's just make it happen automatically.
    
    * No need to explicitly refresh in scale_entity
    
    It now happens automatically

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -1672,7 +1672,9 @@ class WorkflowDeploymentContext(context.DeploymentContext):
         :rtype: Modification
         """
         handler = self.workflow_ctx.internal.handler
-        return handler.start_deployment_modification(nodes)
+        modification = handler.start_deployment_modification(nodes)
+        self.workflow_ctx.refresh_node_instances()
+        return modification
 
     @property
     def scaling_groups(self):
