700c7017829e14ae8b594986bda12403f6e3aee6
Mateusz Neumann <mateumann@gmail.com>
2020-06-24 10:14:00 +0200

    CY-2860 Getting rid of status reporter (#492)
    * Getting rid or status-reporter
    
    * Remove ManagerItem.node_id property

--- cloudify_rest_client/manager.py
+++ cloudify_rest_client/manager.py
@@ -161,11 +161,6 @@ class ManagerItem(dict):
         """Content of the CA cert to use for connecting to this manager"""
         return self.get('ca_cert_content')
 
-    @property
-    def node_id(self):
-        """Cloudify's auto-generated node id"""
-        return self.get('node_id')
-
 
 class DBNodeItem(dict):
     def __init__(self, db_node):
@@ -376,8 +371,7 @@ class ManagerClient(object):
             response['metadata']
         )
 
-    def add_broker(self, name, address, port=None, networks=None,
-                   node_id=None):
+    def add_broker(self, name, address, port=None, networks=None):
         """Add a broker to the brokers table.
 
         This will allow cloudify components to use this broker.
@@ -391,7 +385,6 @@ class ManagerClient(object):
             default network with the address parameter. If this is supplied,
             the address in the address parameter must belong to one of the
             networks.
-        :param node_id: Cloudify's auto-generated node id.
 
         :return: The broker that was created.
         """
@@ -403,8 +396,6 @@ class ManagerClient(object):
             params['port'] = port
         if networks:
             params['networks'] = networks
-        if node_id:
-            params['node_id'] = node_id
         response = self.api.post('/brokers', data=params)
         return RabbitMQBrokerItem(response)
 
