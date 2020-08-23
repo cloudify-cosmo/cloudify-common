84b10ef708212a2d84deea23d17c079656b7d765
yuvalr <yuval@cloudify.co>
2020-08-23 14:10:21 +0300

    CY-3098 uncomment all, check

--- cloudify/tests/test_utils.py
+++ cloudify/tests/test_utils.py
@@ -15,7 +15,7 @@
 
 import os
 import mock
-# import shutil
+import shutil
 import logging
 import tempfile
 from testtools import TestCase
@@ -201,4 +201,4 @@ class TestRestUtils(object):
         expected_dir_size = 2 * os_dir_size + 25
         assert rest_utils.get_folder_size_and_files(temp_dir) == \
             (expected_dir_size, 3)
-        # shutil.rmtree(temp_dir)
+        shutil.rmtree(temp_dir)
