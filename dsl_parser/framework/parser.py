d83013939a12cf7b8404d605559abcaf26ad903d
glukhman <michaelg@cloudify.co>
2020-08-24 15:03:29 +0000

    CY-2733 Fix namespaced input list parsing

--- dsl_parser/framework/parser.py
+++ dsl_parser/framework/parser.py
@@ -286,12 +286,16 @@ class Context(object):
                 holder_key, holder_value = holder_element.get_item(k)
                 if hasattr(holder_value, SKIP_NAMESPACE_FLAG):
                     return
-                if k == 'get_input':
+                if k == 'get_input' and not isinstance(v, list):
                     namespaced_value =\
                         utils.generate_namespaced_value(namespace, v)
                     element[k] = namespaced_value
                     holder_element.value[holder_key].value = namespaced_value
                     holder_value.skip_namespace = True
+                elif k == 'get_input' and isinstance(v, list):
+                    if isinstance(v[0], text_type):
+                        element[k][0] =\
+                            utils.generate_namespaced_value(namespace, v[0])
                 elif k == 'get_property' or k == 'get_attribute':
                     set_namespace_node_intrinsic_functions(
                         namespace,
