b58d045909e594b50846e0c2c9d68d5eaf079071
asdf <tehasdf@gmail.com>
2020-09-01 12:51:16 +0200

    CY-3419 get_operations: fetch all operations
    Follow through pagination. Simple as that.

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -1446,8 +1446,16 @@ class RemoteContextHandler(CloudifyWorkflowContextHandler):
 
     def get_operations(self, graph_id):
         client = get_rest_client()
-        operations = client.operations.list(graph_id)
-        return operations
+        ops = []
+        offset = 0
+        while True:
+            operations = client.operations.list(graph_id, _offset=offset)
+            ops += operations.items
+            if len(ops) < operations.metadata.pagination.total:
+                offset += operations.metadata.pagination.size
+            else:
+                break
+        return ops
 
     def update_operation(self, operation_id, state):
         client = get_rest_client()
