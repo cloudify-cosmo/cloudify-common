773ddcd388ec1db9c563fd9b6c7f5dc02e2a0d8d
Yoni Itzhak <yoni@cloudify.co>
2021-02-15 16:27:13 +0200

    RD-1403 Change labels in dsl to list only (#658)

--- dsl_parser/tests/test_labels.py
+++ dsl_parser/tests/test_labels.py
@@ -16,16 +16,17 @@ labels: {}
         yaml = """
 labels:
     key1:
-        value: key1_val1
+        value:
+            - key1_val1
     key2:
         value:
-          - key2_val1
-          - key2_val2
+            - key2_val1
+            - key2_val2
 """
         parsed = self.parse(yaml)
         labels = parsed[constants.LABELS]
         self.assertEqual(2, len(labels))
-        self.assertEqual('key1_val1', labels['key1']['value'])
+        self.assertEqual(['key1_val1'], labels['key1']['value'])
         self.assertEqual(['key2_val1', 'key2_val2'], labels['key2']['value'])
 
     def test_label_is_scanned(self):
@@ -38,24 +39,19 @@ inputs:
 
 labels:
     concat:
-        value: { concat: ['a', 'b'] }
+        value:
+            - { concat: ['a', 'b'] }
     get_input:
-        value: { get_input: a }
+        value:
+            - { get_input: a }
 """
         plan = prepare_deployment_plan(self.parse(yaml))
         labels = plan[constants.LABELS]
-        self.assertEqual('ab', labels['concat']['value'])
-        self.assertEqual('some_value', labels['get_input']['value'])
+        self.assertEqual(['ab'], labels['concat']['value'])
+        self.assertEqual(['some_value'], labels['get_input']['value'])
 
     def test_label_value_get_attribute_fail(self):
-        yaml_1 = """
-tosca_definitions_version: cloudify_dsl_1_3
-
-labels:
-    get_attribute:
-        value: { get_attribute: [ node, attr ] }
-"""
-        yaml_2 = """
+        yaml = """
 tosca_definitions_version: cloudify_dsl_1_3
 
 labels:
@@ -66,6 +62,4 @@ labels:
 """
         message_regex = '.*cannot be a runtime property.*'
         self.assertRaisesRegex(
-            exceptions.DSLParsingException, message_regex, self.parse, yaml_1)
-        self.assertRaisesRegex(
-            exceptions.DSLParsingException, message_regex, self.parse, yaml_2)
+            exceptions.DSLParsingException, message_regex, self.parse, yaml)
