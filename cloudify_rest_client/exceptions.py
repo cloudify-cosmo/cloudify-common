abd78e6ee4c2a75f699307d6dbcbb9e499bc729c
Mohammed AbuAisha <mohammeda@cloudify.co>
2021-04-22 14:49:43 +0300

    RD-2105 Revert handling invalid csys-environment input exception

--- cloudify_rest_client/exceptions.py
+++ cloudify_rest_client/exceptions.py
@@ -289,14 +289,6 @@ class DeploymentParentNotFound(CloudifyClientError):
     ERROR_CODE = 'deployment_parent_not_found_error'
 
 
-class InvalidCSYSEnvironmentInput(CloudifyClientError):
-    """
-    Raised when deployment input value for `csys-environment` reference parent
-    that does not exist
-    """
-    ERROR_CODE = 'invalid_csys_environment_error'
-
-
 ERROR_MAPPING = dict([
     (error.ERROR_CODE, error)
     for error in [
@@ -330,5 +322,4 @@ ERROR_MAPPING = dict([
         ExpiredCloudifyLicense,
         InvalidFilterRule,
         DeploymentParentNotFound,
-        InvalidCSYSEnvironmentInput,
     ]])
