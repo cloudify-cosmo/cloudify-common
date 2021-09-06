42215a2a1f8e0b8246475acad441a3b744974d14
Mateusz Neumann <mateumann@gmail.com>
2021-09-06 14:25:47 +0200

    Add some docstrings

--- cloudify_rest_client/audit_log.py
+++ cloudify_rest_client/audit_log.py
@@ -2,6 +2,10 @@ from cloudify_rest_client.responses import ListResponse
 
 
 class AuditLog(dict):
+    """AuditLog describes a single entry in the `audit_log` table.  The table
+    is used to to log `operation`s ('create', 'update', 'delete') performed on
+    most of the database tables.  Fields `ref_table` and `ref_id` are used to
+    address referenced records."""
 
     def __init__(self, audit_log):
         super(AuditLog, self).__init__()
@@ -9,26 +13,32 @@ class AuditLog(dict):
 
     @property
     def ref_table(self):
+        """Referenced table name."""
         return self['ref_table']
 
     @property
     def ref_id(self):
+        """Row ID in the referenced table."""
         return self['ref_id']
 
     @property
     def operation(self):
+        """Operation performed on the row: 'create', 'update' or 'delete'."""
         return self['operation']
 
     @property
     def creator_name(self):
+        """Username of the author of the change (if any)."""
         return self['creator_name']
 
     @property
     def execution_id(self):
+        """Execution ID which performed the `operation`."""
         return self['execution_id']
 
     @property
     def created_at(self):
+        """Operation timestamp."""
         return self['created_at']
 
 
