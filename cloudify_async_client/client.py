ac38185a5324d6979731d7b547f9c18025d582a2
Mateusz Neumann <mateumann@gmail.com>
2021-09-27 17:35:46 +0200

    Resolve Lukasz's remarks

--- cloudify_async_client/client.py
+++ cloudify_async_client/client.py
@@ -25,21 +25,20 @@ class CloudifyAsyncClient:
         return '{0}://{1}:{2}/api/{3}'.format(self.protocol, self.host,
                                               self.port, self.api_version)
 
-    def get(self, url, **kwargs):
+    def get(self, url, params=None, **kwargs):
         timeout = kwargs.pop('timeout', 300)
         if timeout is None:
             timeout = 300
         if isinstance(timeout, int) or isinstance(timeout, float):
             timeout = aiohttp.ClientTimeout(total=timeout)
 
-        # Format query parameters
-        params = kwargs.copy()
-        p = {k: str(v) for k, v in params.pop('params', {}).items()
-             if v is not None}
-        if p:
-            params['params'] = p
+        if params:
+            # Format query parameters and pass params only if it is not empty
+            p = {k: str(v) for k, v in params.items() if v is not None}
+            if p:
+                kwargs['params'] = p
 
         return self.session.get(f"{self.url}/{url}",
                                 ssl=self.ssl,
                                 timeout=timeout,
-                                **params)
+                                **kwargs)
