996967d57d5f3c528d7055e51cd53405e7e70df7
asdf <tehasdf@gmail.com>
2021-10-27 13:57:49 +0200

    Remove duplicated except branch
    It's identical to the BaseException case, which covers this one
    as well obviously.

--- cloudify/dispatch.py
+++ cloudify/dispatch.py
@@ -249,9 +249,6 @@ class WorkflowHandler(TaskHandler):
             else:
                 self._workflow_succeeded()
             return result
-        except exceptions.WorkflowFailed as e:
-            self._workflow_failed(e, traceback.format_exc())
-            raise
         except BaseException as e:
             self._workflow_failed(e, traceback.format_exc())
             raise
