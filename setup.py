3b4b6e9cdd3a19bb0b8571e242e5914ecc9a6bba
opencm <limor@gigaspaces.com>
2022-02-13 14:26:27 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ except ImportError:
 
 setup(
     name='cloudify-common',
-    version='6.3.1.dev1',
+    version='6.3.1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(exclude=('dsl_parser.tests*',
