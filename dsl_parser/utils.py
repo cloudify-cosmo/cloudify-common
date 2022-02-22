41913fe75cf2885694423132c699e1d1e0bf2697
Mateusz Neumann <mateumann@gmail.com>
2022-02-22 16:00:51 +0100

    RD-4122 Add `blueprint_id` DSL data type

--- dsl_parser/utils.py
+++ dsl_parser/utils.py
@@ -199,7 +199,7 @@ def parse_value(
     elif type_name == 'boolean':
         if isinstance(value, bool):
             return value
-    elif type_name in ('string', 'secret', 'deployment_id'):
+    elif type_name in ('string', 'secret', 'deployment_id', 'blueprint_id'):
         return value
     elif type_name == 'regex':
         if isinstance(value, text_type):
