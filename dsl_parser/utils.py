deaf32657debd9e2e039fe83a6fb87d8bdd11122
Mateusz Neumann <mateumann@gmail.com>
2022-03-16 13:39:14 +0100

    RD-4135 Define `secret_id` data type (#1013)

--- dsl_parser/utils.py
+++ dsl_parser/utils.py
@@ -200,7 +200,8 @@ def parse_value(
         if isinstance(value, bool):
             return value
     elif type_name in ('string', 'secret', 'textarea',
-                       'deployment_id', 'blueprint_id', 'capability_value'):
+                       'deployment_id', 'blueprint_id', 'secret_key',
+                       'capability_value'):
         return value
     elif type_name == 'regex':
         if isinstance(value, text_type):
