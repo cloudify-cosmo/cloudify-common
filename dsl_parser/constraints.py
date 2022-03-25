78937319d2099ac458467cd41dcdec79c085cbf1
Mateusz Neumann <mateumann@gmail.com>
2022-03-25 21:44:54 +0100

    RD-4169,  RD-4170 DSL data type `node_type` (#1027)
    * Implement REST NodeTypesClient
    
    * Add a new `node_type` data type declaration

--- dsl_parser/constraints.py
+++ dsl_parser/constraints.py
@@ -200,7 +200,7 @@ class InRange(Constraint):
 
 @register_constraint(name='valid_values', constraint_data_type=_SEQUENCE)
 class ValidValues(DataBasedConstraint):
-    SUPPORTED_DATA_TYPES = ['capability_value', 'node_id']
+    SUPPORTED_DATA_TYPES = ['capability_value', 'node_id', 'node_type']
 
     def predicate(self, value):
         return _try_predicate_func(
@@ -285,7 +285,7 @@ class Tenants(DataBasedConstraint):
 class NamePattern(DataBasedConstraint):
     SUPPORTED_DATA_TYPES = ['deployment_id', 'blueprint_id',
                             'capability_value', 'secret_key',
-                            'node_id']
+                            'node_id', 'node_type']
 
     def query_param(self, data_type=None):
         if data_type == 'blueprint_id':
@@ -298,6 +298,8 @@ class NamePattern(DataBasedConstraint):
             return 'capability_key_specs'
         elif data_type == 'node_id':
             return 'id_specs'
+        elif data_type == 'node_type':
+            return 'type_specs'
         else:
             raise NotImplementedError(
                 "'{0}' constraint is not implemented for data type '{1}'"
@@ -307,7 +309,7 @@ class NamePattern(DataBasedConstraint):
 
 @register_constraint(name='deployment_id', constraint_data_type=_STRING)
 class DeploymentId(DataBasedConstraint):
-    SUPPORTED_DATA_TYPES = ['capability_value', 'node_id']
+    SUPPORTED_DATA_TYPES = ['capability_value', 'node_id', 'node_type']
 
 
 @register_validation_func(constraint_data_type=_SCALAR)
@@ -386,7 +388,7 @@ def validate_input_value(input_name, input_constraints, input_value,
             'function and also have '
             'constraints.'.format(input_value, input_name))
 
-    if type_name in ['capability_value', 'node_id'] \
+    if type_name in ['capability_value', 'node_id', 'node_type'] \
             and 'deployment_id' not in {c.name for c in input_constraints}:
         raise exceptions.ConstraintException(
             "Input '{0}' of type '{1}' lacks 'deployment_id' constraint."
