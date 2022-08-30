0f71f4eb49e8319de6ace87d3bb8213adb40859c
Mateusz Neumann <mateumann@gmail.com>
2022-08-30 15:03:41 +0200

    RD-5662 Validate DSL version for data-based types in node properties (#1131)

--- dsl_parser/elements/data_types.py
+++ dsl_parser/elements/data_types.py
@@ -59,19 +59,6 @@ class SchemaPropertyType(Element):
 
     provides = ['component_types']
 
-    def validate(self, data_type, **kwargs):
-        if self.initial_value and self.initial_value not in \
-                constants.USER_PRIMITIVE_TYPES and not data_type:
-            raise exceptions.DSLParsingLogicException(
-                exceptions.ERROR_UNKNOWN_TYPE,
-                "Illegal type name '{0}'".format(self.initial_value))
-
-    def calculate_provided(self, component_types, **kwargs):
-        return {'component_types': component_types}
-
-
-class SchemaInputType(SchemaPropertyType):
-
     def validate(self, data_type, version=None, validate_version=None,
                  **kwargs):
         if self.initial_value and self.initial_value not in \
@@ -82,6 +69,13 @@ class SchemaInputType(SchemaPropertyType):
         if validate_version and self.initial_value and \
                 self.initial_value in constants.TYPES_BASED_ON_DB_ENTITIES:
             self.validate_version(version, (1, 4))
+
+    def calculate_provided(self, component_types, **kwargs):
+        return {'component_types': component_types}
+
+
+class SchemaInputType(SchemaPropertyType):
+    pass
 
 
 class SchemaListItemType(SchemaPropertyType):
@@ -308,6 +302,8 @@ SchemaPropertyType.requires[DataType] = [
     Value('data_type', predicate=_has_type, required=False),
     Requirement('component_types', predicate=_has_type, required=False)
 ]
+SchemaPropertyType.requires[_version.ToscaDefinitionsVersion] = ['version']
+SchemaPropertyType.requires['inputs'] = ['validate_version']
 SchemaInputType.requires[DataType] = [
     Value('data_type', predicate=_has_type, required=False),
     Requirement('component_types', predicate=_has_type, required=False)
