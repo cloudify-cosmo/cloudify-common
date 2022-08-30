962a83101622973e89e20e0ad83e450206e22de5
geokala <geokala@users.noreply.github.com>
2022-08-30 15:26:29 +0100

    Set empty dicts, and support setting executions (#1132)
    * Set empty dicts, and support setting executions
    
    * Use set_attributes instead of set_executions
    
    * Update cloudify_rest_client/deployments.py
    
    see pep 257
    
    * Eat whitespace
    
    Co-authored-by: Lukasz Maksymczuk <tehasdf@users.noreply.github.com>

--- cloudify_rest_client/deployments.py
+++ cloudify_rest_client/deployments.py
@@ -776,39 +776,39 @@ class DeploymentsClient(object):
         assert blueprint_id
         assert deployment_id
         data = {'blueprint_id': blueprint_id, 'visibility': visibility}
-        if inputs:
+        if inputs is not None:
             data['inputs'] = inputs
-        if site_name:
+        if site_name is not None:
             data['site_name'] = site_name
-        if labels:
+        if labels is not None:
             data['labels'] = labels
-        if display_name:
+        if display_name is not None:
             data['display_name'] = display_name
-        if _workdir_zip:
+        if _workdir_zip is not None:
             data['workdir_zip'] = _workdir_zip
-        if workflows:
+        if workflows is not None:
             data['workflows'] = workflows
-        if groups:
+        if groups is not None:
             data['groups'] = groups
-        if scaling_groups:
+        if scaling_groups is not None:
             data['scaling_groups'] = scaling_groups
-        if policy_triggers:
+        if policy_triggers is not None:
             data['policy_triggers'] = policy_triggers
-        if policy_types:
+        if policy_types is not None:
             data['policy_types'] = policy_types
-        if outputs:
+        if outputs is not None:
             data['outputs'] = outputs
-        if capabilities:
+        if capabilities is not None:
             data['capabilities'] = capabilities
-        if resource_tags:
+        if resource_tags is not None:
             data['resource_tags'] = resource_tags
-        if outputs:
+        if outputs is not None:
             data['outputs'] = outputs
-        if description:
+        if description is not None:
             data['description'] = description
-        if deployment_status:
+        if deployment_status is not None:
             data['deployment_status'] = deployment_status
-        if installation_status:
+        if installation_status is not None:
             data['installation_status'] = installation_status
         data['skip_plugins_validation'] = skip_plugins_validation
         data['runtime_only_evaluation'] = runtime_only_evaluation
@@ -904,11 +904,12 @@ class DeploymentsClient(object):
         return Deployment(updated_dep)
 
     def set_attributes(self, deployment_id, **kwargs):
-        """Set kwargs on the deployment.
+        """Set arbitrary properties on the deployment.
 
-        This is used internally for first populating the deployment
-        with the attributes from the plan.
-        For updating existing deployments, use the deployment update methods.
+        If you're not sure, you probably want to look at deployment update
+        instead.
+
+        For internal use only.
         """
         updated_dep = self.api.patch(
             '/deployments/{0}'.format(deployment_id), data=kwargs)
