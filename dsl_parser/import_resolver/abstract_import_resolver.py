58d16cc738b3ea67b1bf2fc0e1df7eabaf6cea7f
Mateusz Neumann <mateumann@gmail.com>
2022-11-18 09:26:46 +0100

    Revert "Run linter on imported YAMLs"
    This reverts commit b5ebca4ae5d8437328f7cb7cd33773d78a23002e.

--- dsl_parser/import_resolver/abstract_import_resolver.py
+++ dsl_parser/import_resolver/abstract_import_resolver.py
@@ -19,7 +19,6 @@ import time
 import requests
 
 from dsl_parser import exceptions
-from dsl_parser.linter import validate
 
 
 DEFAULT_RETRY_DELAY = 1
@@ -47,11 +46,8 @@ class AbstractImportResolver(abc.ABC):
 
     def fetch_import(self, import_url, **kwargs):
         if is_remote_resource(import_url):
-            document = self.resolve(import_url)
-        else:
-            document = read_import(import_url)
-        validate(document, import_url)
-        return document
+            return self.resolve(import_url)
+        return read_import(import_url)
 
     @abc.abstractmethod
     def retrieve_plugin(self, import_url, **kwargs):
