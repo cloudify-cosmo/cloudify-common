e24a4c1198a7664c304673dc6481c237f8e7ffa8
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2022-12-06 12:10:48 +0100

    agents list: also no need to constraint include
    We maybe don't always want to fetch credentials, but a more modern way
    to achieve that would be to use `get_data` - so that's what I'm
    doing in cloudify-cosmo/cloudify-manager#3958
    
    The problem with hardcoding the default include in here is that it
    won't have any newer fields. Specifically, this one is missing `state`.

--- cloudify_rest_client/agents.py
+++ cloudify_rest_client/agents.py
@@ -154,12 +154,6 @@ class AgentsClient(object):
         if sort:
             kwargs['_sort'] = '-' + sort if is_descending else sort
 
-        if _include is None:
-            # Default the _include to what was available before, to avoid
-            # defaulting to including credentials, etc
-            _include = ['id', 'host_id', 'ip', 'install_method', 'system',
-                        'version', 'node', 'deployment', 'tenant_name']
-
         response = self.api.get('/{self._uri_prefix}'.format(self=self),
                                 _include=_include,
                                 params=kwargs)
