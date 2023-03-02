971b2b762843d31ea6f357b95176e6ca12a06d7f
Mateusz Neumann <mateumann@gmail.com>
2023-03-02 10:49:54 +0100

    More precise treatment of IDD's attributes
    * docstrings
    * attribute (name) validation

--- cloudify/deployment_dependencies.py
+++ cloudify/deployment_dependencies.py
@@ -20,13 +20,44 @@ TARGET_DEPLOYMENT_FUNC = 'target_deployment_func'
 EXTERNAL_SOURCE = 'external_source'
 EXTERNAL_TARGET = 'external_target'
 
+VALID_ATTRIBUTES = [DEPENDENCY_CREATOR, SOURCE_DEPLOYMENT, TARGET_DEPLOYMENT,
+                    TARGET_DEPLOYMENT_FUNC, EXTERNAL_SOURCE, EXTERNAL_TARGET,
+                    'id', 'visibility', 'created_at', 'created_by']
+
 
 def build_deployment_dependency(dependency_creator, **kwargs) -> dict:
-    """Build dict representation of the inter-deployment dependency."""
+    """Build dict representation of the inter-deployment dependency.
+
+    :param dependency_creator: A string representing the entity that is
+                               responsible for this dependency (e.g. an
+                               intrinsic function, blueprint path,
+                               'node_instances.some_node_instance', etc.).
+    :param source_deployment: Source deployment that depends on the target
+                              deployment.
+    :param target_deployment: The deployment that the source deployment
+                              depends on.
+    :param target_deployment_func: A function used to determine the target
+                                   deployment.
+    :param external_source: If the source deployment uses an external resource
+                            as target, pass here a JSON containing the source
+                            deployment metadata, i.e. deployment name, tenant
+                            name, and the manager host(s).
+    :param external_target: If the source deployment uses an external resource
+                            as target, pass here a JSON containing the target
+                            deployment metadata, i.e. deployment name, tenant
+                            name, and the manager host(s).
+    :param id: Override the identifier. Internal use only.
+    :param visibility: Override the visibility. Internal use only.
+    :param created_at: Override the creation timestamp. Internal use only.
+    :param created_by: Override the creator. Internal use only.
+    """
     dependency = {
         DEPENDENCY_CREATOR: dependency_creator,
     }
     for key, value in kwargs.items():
+        if key not in VALID_ATTRIBUTES:
+            raise RuntimeError('Not valid inter-deployment dependency '
+                               f'attribute: {key}')
         if value is not None:
             dependency[key] = value
     return dependency
