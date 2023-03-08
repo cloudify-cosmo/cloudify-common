bd265d3dad35a6cdc86a0c28c8cf836b28d508ea
opencm <limor@gigaspaces.com>
2023-03-08 09:54:45 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudify-common',
-    version='7.0.0.dev1',
+    version='7.0.0',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(exclude=('cloudify.tests*',
