8c7473d6db63a50cbfb3ed8d998ec815a1204a88
opencm <limor@gigaspaces.com>
2023-08-02 15:48:39 +0000

    Bump version

--- packaging/plugins/provision.sh
+++ packaging/plugins/provision.sh
@@ -124,8 +124,8 @@ function wagon_create_package(){
 }
 
 # VERSION/PRERELEASE/BUILD must be exported as they is being read as an env var by the cloudify-agent-packager
-export CORE_TAG_NAME="7.0.2.dev1"
-export CORE_BRANCH="7.0.2-build"
+export CORE_TAG_NAME="7.0.2-build"
+export CORE_BRANCH="7.0.2-build-build"
 curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-common/$CORE_BRANCH/packaging/common/provision.sh -o ./common-provision.sh &&
 source common-provision.sh
 
