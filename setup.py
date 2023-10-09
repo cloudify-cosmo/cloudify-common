a24f9575a665328fba58e91b56269d8d0c7b48a8
glukhman <47591609+glukhman@users.noreply.github.com>
2023-10-09 18:23:39 +0300

    NE-7622 Upgrade dependencies (#1314)
    Co-authored-by: glukhman <ubuntu@ip-172-31-41-57.eu-west-1.compute.internal>

--- setup.py
+++ setup.py
@@ -1,31 +1,37 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='cloudify-common',
     version='7.0.3.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
-    packages=find_packages(exclude=('cloudify.tests*',
-                                    'cloudify_rest_client.tests*',
-                                    'dsl_parser.tests*',
-                                    'script_runner.tests*',)),
+    packages=find_packages(
+        exclude=(
+            'cloudify.tests*',
+            'cloudify_rest_client.tests*',
+            'dsl_parser.tests*',
+            'script_runner.tests*',
+        )
+    ),
     include_package_data=True,
     license='LICENSE',
     description='Cloudify Common',
     zip_safe=False,
     install_requires=[
-        'proxy_tools==0.1.0',
-        'bottle==0.12.23',
+        'aiohttp>=3.8.5,<4',
+        'bottle<1',
+        'distro>=1.7.0,<2',
+        'fasteners<1',
         'jinja2>3,<4',
-        'requests_toolbelt==0.9.1',
-        'wagon>=0.12',
-        'pytz==2022.2.1',
-        'pika==1.3.0',
-        'requests>=2.27.1,<3.0.0',
-        'fasteners==0.17.3',
-        'distro>=1.7.0',
-        'aiohttp>3,<4',
+        'pika<2',
+        'proxy_tools<1',
+        'pytz',
+        'requests<3',
+        'requests_toolbelt>=1,<2',
+        'wagon>=1,<2',
     ],
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'ctx = cloudify.proxy.client:main',
@@ -33,7 +39,7 @@ setup(
     },
     package_data={'cloudify.ctx_wrappers': ['ctx.py']},
     scripts=[
-        'ctx_wrappers/ctx-sh'
+        'ctx_wrappers/ctx-sh',
     ],
     extras_require={
         # for running workflows (in the mgmtworker and the cli), as opposed
@@ -43,7 +49,7 @@ setup(
             'networkx>2,<3',
         ],
         'snmp': [
-            'pysnmp==4.4.5'
+            'pysnmp==4.4.5',
         ]
     }
 )
