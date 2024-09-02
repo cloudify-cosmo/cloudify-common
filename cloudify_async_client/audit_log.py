9de732f51fe29c4b4f42b79729af60b656b858f0
Jakub Stepien <jakub.kuba.stepien@gmail.com>
2024-09-02 18:04:32 +0200

    Upgrade aiohttp to 3.10.5
    Add context manager for AuditLogAsyncClient
    and manage closing of the AsyncClient Session

--- cloudify_async_client/audit_log.py
+++ cloudify_async_client/audit_log.py
@@ -3,6 +3,15 @@ from cloudify_rest_client.audit_log import AuditLogClient
 
 
 class AuditLogAsyncClient(AuditLogClient):
+    _client = None
+
+    async def __aenter__(self):
+        self._client = await self.async_client()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+
     async def stream(self, timeout=None, **kwargs):
         """
         Returns a list of AuditLogs.
@@ -14,11 +23,17 @@ class AuditLogAsyncClient(AuditLogClient):
         :return:        ``ListResponse`` with of ``AuditLog`` items and
                         response metadata.
         """
-        client = await self.async_client()
-        response = await client.get('audit/stream',
-                                    params=kwargs,
-                                    timeout=timeout)
+        if not self._client:
+            self._client = await self.async_client()
+        response = await self._client.get(
+            'audit/stream',
+            params=kwargs,
+            timeout=timeout
+        )
         return response
+
+    async def close(self):
+        await self._client.close_session()
 
     async def async_client(self):
         headers = self.api.headers.copy()
--- cloudify_async_client/client.py
+++ cloudify_async_client/client.py
@@ -9,10 +9,13 @@ class CloudifyAsyncClient:
     cert: str
 
     def __init__(self, **kwargs):
-        # only import aiohttp if this is used - otherwise we pay the price
+        # only import asyncio and aiohttp if this is used
+        # otherwise we pay the price
         # on every import of the rest-client
+        import asyncio
         import aiohttp
         self._aiohttp = aiohttp
+        self._asyncio = asyncio
 
         self.host = kwargs.pop('host', 'localhost')
         self.port = kwargs.pop('port', 443)
@@ -21,14 +24,29 @@ class CloudifyAsyncClient:
         self.cert = kwargs.pop('cert')
         self.ssl = ssl.create_default_context(cafile=self.cert)
         self.api_version = 'v3.1'
-        self.session = self._aiohttp.ClientSession(headers=self.headers)
+        self._session = None
 
     @property
     def url(self):
         return '{0}://{1}:{2}/api/{3}'.format(self.protocol, self.host,
                                               self.port, self.api_version)
 
-    def get(self, url, params=None, timeout=300, **kwargs):
+    @property
+    def session(self):
+        if not self._session:
+            self._session = self._aiohttp.ClientSession(
+                headers=self.headers,
+                loop=self._asyncio.get_event_loop(),
+            )
+        return self._session
+
+    async def close_session(self):
+        if self._session and not self._session.closed:
+            await self._session.close()
+
+    async def get(self, url, params=None, timeout=300, **kwargs):
+        # only import aiohttp if this is used - otherwise we pay the price
+        # on every import of the rest-client
         if isinstance(timeout, int) or isinstance(timeout, float):
             timeout = self._aiohttp.ClientTimeout(total=timeout)
 
@@ -38,7 +56,10 @@ class CloudifyAsyncClient:
             if p:
                 kwargs['params'] = p
 
-        return self.session.get(f"{self.url}/{url}",
-                                ssl=self.ssl,
-                                timeout=timeout,
-                                **kwargs)
+        response = await self.session.get(
+            f"{self.url}/{url}",
+            ssl=self.ssl,
+            timeout=timeout,
+            **kwargs
+        )
+        return response
